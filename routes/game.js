a44a1ed977ad28df1906b414ff0677b009caaef7
Brian E Tovar <betovar@leaflabs.com>
2013-03-14 16:29:19 -0400

    turning game.js into an object design pattern
    Signed-off-by: Brian E Tovar <betovar@leaflabs.com>

--- routes/game.js
+++ routes/game.js
@@ -3,17 +3,42 @@
  */
 
 var random = require('node-random') 
-  , chits = [5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11] 
+  , shasum = require('crypto').createHash('sha1')
+  , chits = [ 5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11 ] 
   , hexes = [ 
     "quarry", "fields", "forest", "quarry",
     "fields", "pasture", "fields", "pasture",
     "forest", "hills", "desert", "hills",
     "pasture", "pasture", "forest", "hills",
-    "quarry", "forest", "fields"] 
-  , ports = [
+    "quarry", "forest", "fields" ] 
+  , harbors = [
     "generic", "lumber", "brick", "generic", 
-    "generic", "wool", "generic", "ore", "grain"]
-  , devcards = [ 
+    "generic", "wool", "generic", "ore", "grain" ];
+
+exports.game = {
+  'id': 0,
+  'start': new Date(),
+  'title': "Settlers of Caten", 
+  'size': "basic", 
+  'lang': "en", 
+  'shapes': { 
+    'port': {cir:{cx:86,r:50},rec:{y:-20,w:50,h:40,rx:3}}, 
+    'hex': "86.6,50 0,100 -86.6,50 -86.6,-50 0,-100 86.6,-50", 
+    'road': "M 0 -35 L 0 35", 
+    'town': "-15,15 -15,-5 0,-15 15,-5 15,15", 
+    'city': "-15,-5 -15,-15 0,-25 15,-15 15,-5 30,-5 30,15 -30,15 -30,-5"
+  }, 
+  'ports': [ 
+    {x:-173.2, y:300, angle:120 }, 
+    {x:0, y:300, angle:60 }, 
+    {x:259.8, y:150, angle:60 }, 
+    {x:346.4, y:0, angle:0 }, 
+    {x:259.8, y:-150, angle:-60 }, 
+    {x:0, y:-300, angle:-60 }, 
+    {x:-173.2, y:-300, angle:-120 }, 
+    {x:-259.8, y:-150, angle:180 }, 
+    {x:-259.8, y:150, angle:180 } ], 
+  'devcardstack': [ 
     {type:"knight"}, {type:"knight"}, 
     {type:"knight"}, {type:"knight"}, 
     {type:"knight"}, {type:"knight"}, 
@@ -31,70 +56,41 @@ var random = require('node-random')
     {type:"victory", title:"library"},
     {type:"victory", title:"market"},
     {type:"victory", title:"palace"},
-    {type:"victory", title:"university"}
-  ];
-
-exports.new = function( options ) { 
-  var shasum = require('crypto').createHash('sha1') 
-  , game = {
-    'id': 0,
-    'start': new Date(),
-    'title': "Settlers of Caten", 
-    'size': "basic", 
-    'lang': "en", 
-    'shapes': {}, 
-    'ports': [ 
-      {x:-173.2, y:300, angle:120 }, 
-      {x:0, y:300, angle:60 }, 
-      {x:259.8, y:150, angle:60 }, 
-      {x:346.4, y:0, angle:0 }, 
-      {x:259.8, y:-150, angle:-60 }, 
-      {x:0, y:-300, angle:-60 }, 
-      {x:-173.2, y:-300, angle:-120 }, 
-      {x:-259.8, y:-150, angle:180 }, 
-      {x:-259.8, y:150, angle:180 } ], 
-    'grid': []
-  }; 
-
-  shuffle(devcards);
-  if (options.hexes) shuffle(hexes); 
-  if (options.chits) shuffle(chits); 
-  if (options.ports) shuffle(ports); 
-  for (var i in game.ports) { 
-    game.ports[i]['flavor'] = ports[i];
-  }
-
-  random.strings({ 
-    "length": 20, 
-    "number": 10 }, function(err, data) { 
+    {type:"victory", title:"university"} ],
+  'board': [], 
+  'settlements': [], 
+  'clients': [],
+  'new': function( options ) { 
+    random.strings({ 
+      "length": 20, 
+      "number": 10 
+    }, function(err, data) { 
       if (err) throw err; 
-      game['id'] = shasum.update(data.join()).digest("hex"); 
+      game.id = shasum.update(data.join()).digest("hex"); 
     }); 
-  game['shapes'] = { 
-    'port': {cir:{cx:86,r:50},rec:{y:-20,w:50,h:40,rx:3}},
-    'hex': "86.6,50 0,100 -86.6,50 -86.6,-50 0,-100 86.6,-50",
-    'road': "M 0 -35 L 0 35",
-    'town': "-15,15 -15,-5 0,-15 15,-5 15,15",
-    'city': "-15,-5 -15,-15 0,-25 15,-15 15,-5 30,-5 30,15 -30,15 -30,-5"
-  };
-  var temp = hexgrid([0,1]); 
-  for (var i=0, len=temp.length; i<len; i++) { 
-    game['grid'].push({ 
-      'x': Math.round(temp[i].x*10)/10, 
-      'y': Math.round(temp[i].y*10)/10 
-    }); 
-  } 
- 
-  for (var i=0, dsrt=0, len=hexes.length; i<len; i++) { 
-    game.grid[i]['resource'] = hexes[i]; 
-    if (hexes[i] != 'desert') game.grid[i]['chit'] = chits[i-dsrt]; 
-    else dsrt += 1; 
-    if (chits[i-dsrt] == 6) game.grid[i]['color'] = "maroon"; 
-    if (chits[i-dsrt] == 8) game.grid[i]['color'] = "maroon"; 
-  } 
-
-  return game; 
-}
+    game.start = new Date();
+    shuffle(game.devcards);
+    if (options.hexes) shuffle(game.hexes); 
+    if (options.chits) shuffle(game.chits); 
+    if (options.ports) shuffle(game.ports); 
+    for (var i in game.ports) { 
+      game.ports[i]['flavor'] = harbors[i];
+    }
+    for (var temp=hexgrid([0,1]), len=temp.length, i=0; i<len; i++) { 
+      game['board'].push({ 
+        'x': Math.round(temp[i].x*10)/10, 
+        'y': Math.round(temp[i].y*10)/10 
+      }); 
+    } 
+    for (var dsrt=0, len=hexes.length, i=0; i<len; i++) { 
+      game.board[i]['resource'] = hexes[i]; 
+      if (hexes[i] != 'desert') game.board[i]['chit'] = chits[i-dsrt]; 
+      else dsrt += 1; 
+      if (chits[i-dsrt] == 6) game.board[i]['color'] = "maroon"; 
+      if (chits[i-dsrt] == 8) game.board[i]['color'] = "maroon"; 
+    } 
+  }
+}; 
 
 /*
  *  Generates points for a hexagonal grid
@@ -164,15 +160,12 @@ function hexgrid( layers ) {
 function shuffle( list ) { 
   random.sequences({ 
     "minimum": 0, 
-    "maximum": list.length-1 } 
-  , function(error, data) { 
+    "maximum": list.length-1 
+  }, function(error, data) { 
     if (error) throw error; 
     console.log(data); 
   }); 
-  for (var i=list.length; i--; ) { 
-    var j = Math.floor( (i+1)*Math.random() ); 
-    var temp = list[j]; 
-    list[j] = list[i]; 
-    list[i] = temp; 
+  for (var temp=list.slice(0), i=list.length; i--; ) { 
+    list[data[i]] = temp[i]; 
   }
 }
