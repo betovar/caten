096c9f85831843a51d4fc981530254b4fbcd4e7b
Brian E Tovar <betovar@leaflabs.com>
2013-07-17 05:40:45 -0400

    added new game form page for creating games fixed bug in twitter auth decided on mobile only layout
    Signed-off-by: Brian E Tovar <betovar@leaflabs.com>

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // Web Server Initialization
-var express = require('express')
-  , app = express()
-  , server = require('http').createServer(app)
-  , passport = require('passport')
-  , path = require('path');
+var express = require('express'),
+  app = express(),
+  server = require('http').createServer(app),
+  passport = require('passport'),
+  path = require('path');
 
 app.configure(function() {
   app.set('views', path.join(__dirname, './views'));
   app.set('view engine', 'jade');
   app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.favicon(path.join(__dirname, 'public/favicon.ico'))); 
-  app.use(express.favicon('public/favicon.ico')); 
+  app.use(express.favicon(path.join(__dirname, 'public/favicon.ico')));
+  app.use(express.favicon('public/favicon.ico'));
   app.use(express.logger('dev'));
   app.use(express.cookieParser());
   app.use(express.bodyParser());
@@ -25,14 +25,15 @@ app.configure('development', function() {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
   Error.stackTraceLimit = Infinity;
   app.set('port', 8080);
+  app.set('URI', "http://127.0.0.1:"+app.get('port')); //necessary for twitter
 });
 app.configure('production', function() {
   app.use(express.errorHandler());
+  app.set('URI', "http://"+process.env.SUBDOMAIN+".jit.su");
 });
 server.listen(app.get('port'), function() {
-  console.log('Express server running in %s mode on port %s' 
-    , app.get('env')
-    , app.get('port')
+  console.log('Express server running in %s mode on port %s',
+    app.get('env'), app.get('port')
   );
 });
 
@@ -42,24 +43,21 @@ var twitter = require('passport-twitter').Strategy;
 passport.use(new twitter({
   consumerKey: process.env.TWITTER_CONSUMER_KEY,
   consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-  callbackURL: process.env.TWITTER_CALLBACK_URL
+  callbackURL: app.get('URI')+"/auth/twitter/callback"
 }, function(token, tokenSecret, profile, done) {
-  // asynchronous verification, for effect...
-  process.nextTick(function () {
-    // To keep the example simple, the user's Twitter profile is returned to
-    // represent the logged-in user.  In a typical application, you would want
-    // to associate the Twitter account with a user record in your database,
-    // and return that user instead.
-    return done(null, profile);
-  });
+  // To keep the example simple, the user's Twitter profile is returned to
+  // represent the logged-in user.  In a typical application, you would want
+  // to associate the Twitter account with a user record in your database,
+  // and return that user instead.
+  console.log(profile);
+  return done(null, profile);
 }));
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
+  done(null, user);
 });
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
 });
 // Simple route middleware to ensure user is authenticated.
 //   Use this route middleware on any resource that needs to be protected.  If
@@ -68,45 +66,46 @@ passport.deserializeUser(function(id, done) {
 //   login page.
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+  res.redirect('/login');
 }
 
 // Route Definitions
-app.get('/auth/twitter/callback', 
-  passport.authenticate('twitter', { 
-    failureRedirect: '/login' 
-  }), function(req, res) { 
-    console.log(req.query.oauth_token); 
-    res.redirect('/'); 
-  }
-);
+app.get('/auth/twitter/callback',
+  //NB: https://dev.twitter.com/issues/824
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log(req.query);
+    res.redirect('/');
+  });
 app.get('/auth/twitter', passport.authenticate('twitter'));
 app.get('/login', function(req, res) {
-  res.render('/', { user: req.user });
+  res.render('login', {title: 'Login'});
 });
-app.get('/logout', function(req, res) { 
-  req.session = null; 
-  req.logout(); 
-  res.redirect('/'); 
-}); 
-app.get('/:id', function(req, res) { 
-  res.render('game', {title:'Settlers of Caten', gameid: req.params.id}); 
-}); 
-app.get('/', function(req, res) { 
-  res.render('home', {title: 'Settlers of Caten'}); 
-}); 
+app.get('/logout', function(req, res) {
+  //req.session = null;
+  req.logout();
+  res.redirect('/');
+});
+app.get('/form', function(req, res) {
+  res.render('form', {title: 'Create Game'});
+});
+app.get('/:id', function(req, res) {
+  res.render('game', {title:'Caten', gameid: req.params.id});
+});
+app.get('/', function(req, res) {
+  res.render('home', {title: 'Settlers of Caten'});
+});
 
 
-// Socket Handling
-var io = require('socket.io').listen(server) 
-  //, nodemailer = require("nodemailer") 
-  //, redis  = require("redis"),
-  //, client = redis.createClient();
-var game = io.of('').on('connection', function (socket) { 
-  socket.of('/game').on('join', function(room) { 
-    client.auth("somepass");
-    console.log(socket.id + " connected"); 
-    socket.join(room); 
-    io.of('/game').in(room).emit('joined', {username:socket.id}); 
-  }); 
-}); 
+/*// Socket Handling
+var io = require('socket.io').listen(server);
+io.of('').on('connection', function (socket) {
+  socket.on('join', function(room) {
+    socket.join(room);
+    io.of('').in(room).emit('joined', {username:socket.id});
+    console.log(socket.id + " connected");
+  });
+  socket.on('disconnect', function(socket) {
+    io.of('').emit('exited', {username:socket.id});
+  });
+});*/
--- game/index.js
+++ game/index.js
@@ -1,27 +1,78 @@
 /*
- * GET newgame data
+ * new game constructor
  */
 
-var random = require('node-random') 
-  , shasum = require('crypto').createHash('sha1')
-  , chits = [ 5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11 ] 
-  , hexes = [ 
-    "quarry", "fields", "forest", "quarry",
-    "fields", "pasture", "fields", "pasture",
-    "forest", "hills", "desert", "hills",
-    "pasture", "pasture", "forest", "hills",
-    "quarry", "forest", "fields" ] 
-  , harbors = [
-    "generic", "lumber", "brick", "generic", 
-    "generic", "wool", "generic", "ore", "grain" ] 
-  . deck = [ 
-    {type:"knight"}, {type:"knight"}, 
-    {type:"knight"}, {type:"knight"}, 
-    {type:"knight"}, {type:"knight"}, 
-    {type:"knight"}, {type:"knight"}, 
-    {type:"knight"}, {type:"knight"}, 
-    {type:"knight"}, {type:"knight"}, 
-    {type:"knight"}, {type:"knight"}, 
+var random = require('node-random'),
+  db = require('./database.js');
+
+var basic = {
+  'chits': [
+    5,  2,  6,  3,  8, 10,  9, 12, 11,
+    4,  8, 10,  9,  4,  5,  6,  3, 11 ],
+  'geometry': [
+    { x: -173, y: 300 },
+    { x: 0, y: 300 },
+    { x: 173, y: 300 },
+    { x: 260, y: 150 },
+    { x: 346, y: 0 },
+    { x: 260, y: -150 },
+    { x: 173, y: -300 },
+    { x: 0, y: -300 },
+    { x: -173, y: -300 },
+    { x: -260, y: -150 },
+    { x: -346, y: 0 },
+    { x: -260, y: 150 },
+    { x: -87, y: 150 },
+    { x: 87, y: 150 },
+    { x: 173, y: 0 },
+    { x: 87, y: -150 },
+    { x: -87, y: -150 },
+    { x: -173, y: 0 },
+    { x: 0, y: 0 } ],
+  'ports': [
+    {x:-173, y:300, angle:120 },
+    {x:0, y:300, angle:60 },
+    {x:260, y:150, angle:60 },
+    {x:346, y:0, angle:0 },
+    {x:260, y:-150, angle:-60 },
+    {x:0, y:-300, angle:-60 },
+    {x:-173.2, y:-300, angle:-120 },
+    {x:-260, y:-150, angle:180 },
+    {x:-260, y:150, angle:180 } ],
+  'ocean': ""
+};
+
+var expansion = {
+  'chits': [
+    2,  5,  4,  6,  3,  9,  8, 11, 11, 10,  6,  3,  8,  4,
+    8, 10, 11, 12, 10,  5,  4,  9,  5,  9, 12,  3,  2,  6 ],
+  'geometry': [],
+  'ports': [],
+  'ocean': ""
+};
+
+  var hexes = [
+    "quarry",  "fields",  "forest",  "quarry",
+    "fields",  "pasture", "fields",  "pasture",
+    "forest",  "hills",   "desert",  "hills",
+    "pasture", "pasture", "forest",  "hills",
+    "quarry",  "forest",  "fields",
+    "desert",  "fields",  "fields",  "forest",
+    "forest",  "pasture", "pasture", "quarry",
+    "quarry",  "hills",   "hills" ],
+  harbor = [
+    "generic", "lumber",  "brick",
+    "generic", "generic", "wool",
+    "generic", "ore",     "grain",
+    "wool",   "generic" ],
+  stack = [
+    {type:"knight"}, {type:"knight"},
+    {type:"knight"}, {type:"knight"},
+    {type:"knight"}, {type:"knight"},
+    {type:"knight"}, {type:"knight"},
+    {type:"knight"}, {type:"knight"},
+    {type:"knight"}, {type:"knight"},
+    {type:"knight"}, {type:"knight"},
     {type:"progress", title:"monopoly"},
     {type:"progress", title:"monopoly"},
     {type:"progress", title:"invention"},
@@ -32,140 +83,102 @@ var random = require('node-random')
     {type:"victory", title:"library"},
     {type:"victory", title:"market"},
     {type:"victory", title:"palace"},
-    {type:"victory", title:"university"} ];
+    {type:"victory", title:"university"},
+    {type:"knight"}, {type:"knight"},
+    {type:"knight"}, {type:"knight"},
+    {type:"knight"}, {type:"knight"},
+    {type:"progress", title:"monopoly"},
+    {type:"progress", title:"invention"},
+    {type:"progress", title:"construction"} ],
+  shapes = {
+    'port': {cir:{cx:86,r:50},rec:{y:-20,w:50,h:40,rx:3}},
+    'hex': "86.6,50 0,100 -86.6,50 -86.6,-50 0,-100 86.6,-50",
+    'road': "M 0 -35 L 0 35",
+    'town': "-15,15 -15,-5 0,-15 15,-5 15,15",
+    'city': "-15,-5 -15,-15 0,-25 15,-15 15,-5 30,-5 30,15 -30,15 -30,-5"
+  };
 
-exports.newgame = function( options ) { 
-  var game = {
-    'id': 0, 
-    'start': new Date(), 
-    'title': "Settlers of Caten", 
-    'size': "basic", 
-    'lang': "en", 
-    'shapes': { 
-      'port': {cir:{cx:86,r:50},rec:{y:-20,w:50,h:40,rx:3}}, 
-      'hex': "86.6,50 0,100 -86.6,50 -86.6,-50 0,-100 86.6,-50", 
-      'road': "M 0 -35 L 0 35", 
-      'town': "-15,15 -15,-5 0,-15 15,-5 15,15", 
-      'city': "-15,-5 -15,-15 0,-25 15,-15 15,-5 30,-5 30,15 -30,15 -30,-5"
-    }, 
-    'ports': [ 
-      {x:-173.2, y:300, angle:120 }, 
-      {x:0, y:300, angle:60 }, 
-      {x:259.8, y:150, angle:60 }, 
-      {x:346.4, y:0, angle:0 }, 
-      {x:259.8, y:-150, angle:-60 }, 
-      {x:0, y:-300, angle:-60 }, 
-      {x:-173.2, y:-300, angle:-120 }, 
-      {x:-259.8, y:-150, angle:180 }, 
-      {x:-259.8, y:150, angle:180 } ],  
-    'board': [], 
-    'settlements': []
-  }; 
-  random.strings({ 
-    "length": 20, 
-    "number": 10 
-  }, function(err, data) { 
-    if (err) throw err; 
-    game.id = shasum.update(data.join()).digest("hex"); 
-  }); 
-  game.start = Date();
-  shuffle(game.devcards);
-  if (options.hexes) shuffle(game.hexes); 
-  if (options.chits) shuffle(game.chits); 
-  if (options.ports) shuffle(game.ports); 
-  for (var i in game.ports) { 
-    game.ports[i]['flavor'] = harbors[i];
+exports.newBoard = function( gameid ) {
+  db.hmset(gameid+':board',
+    'id', gameid,
+    'start', Date(),
+    'locale', "en" //FIXME: to be set by user record
+  );
+  var f = db.hmget(gameid+':form', board);
+  switch (f.board) {
+  case 'expansion':
+    createSequence(gameid, 'hexes', 30);
+    createSequence(gameid, 'chits', 28);
+    createSequence(gameid, 'stack', 34);
+    //createSequence(gameid, 'ports', 11);
+    //createSequence(gameid, 'harbors', 11);
+    break;
+  default:
+    createSequence(gameid, 'hexes', 19);
+    createSequence(gameid, 'chits', 18);
+    createSequence(gameid, 'stack', 25);
+    break;
   }
-  for (var temp=hexgrid([0,1]), len=temp.length, i=0; i<len; i++) { 
-    game['board'].push({ 
-      'x': Math.round(temp[i].x*10)/10, 
-      'y': Math.round(temp[i].y*10)/10 
-    }); 
-  } 
-  for (var dsrt=0, len=hexes.length, i=0; i<len; i++) { 
-    game.board[i]['resource'] = hexes[i]; 
-    if (hexes[i] != 'desert') game.board[i]['chit'] = chits[i-dsrt]; 
-    else dsrt += 1; 
-    if (chits[i-dsrt] == 6) game.board[i]['color'] = "maroon"; 
-    if (chits[i-dsrt] == 8) game.board[i]['color'] = "maroon"; 
-  } 
-  return 'demo';
+  //seat ordering
+  if (f.seat == 'ordered by join') {
+    createSequence(gameid, 'seatorder', f.playercount);
+  }
+  //port configure
+  var port_count = 9;
+  if (f.board == 'expansion') { port_count = 11; }
+  createSequence(gameid, 'ports', port_count);
+  createSequence(gameid, 'harbors', port_count);
+};
+
+exports.newForm = function( form ) {
+  var gameid = newGameID();
+  if (db.exists(gameid)) { return new Error("Game already exists"); }
+  db.hmset(gameid+':form',
+    'board', form.boardsize,
+    'player', form.playercount,
+    'victory', form.victorycondition,
+    'security', form.securitylevel,
+    'placement', form.placementorder,
+    'seat', form.seatorder,
+    'chat', form.keyboardchat,
+    'assist', form.turnassist,
+    'timer', form.turntimer,
+    'title', form.webtitle,
+    'profile', form.profile
+  );
+};
+
+function newGameID() {
+  random.strings({ // an 8 character random string
+    "length": 8,
+    "number": 1
+  }, function(err, data) {
+    if (err) throw err;
+    return data;
+  });
 }
 
-/*
- *  Generates points for a hexagonal grid
- */
-function hexgrid( layers ) {
-  var hexes = [{ x:0, y:0 }]
-  , c = Math.cos(Math.PI/3.0)
-  , s = Math.sin(Math.PI/3.0)
-  , r=173.2;
-  for (var j in layers) { 
-    var last = hexes[0];
-    hexes.unshift({
-      'x': last.x-r,
-      'y': last.y
-    });
-    for (var i=0; i<=j-1; i++) { 
-      last = hexes[0];
-      hexes.unshift({
-        'x': last.x-r*c,
-        'y': last.y-r*s
-      });
-    }
-    for (var i=0; i<=j; i++) { 
-      last = hexes[0];
-      hexes.unshift({
-        'x': last.x+r*c,
-        'y': last.y-r*s
-      });
-    }
-    for (var i=0; i<=j; i++) { 
-      last = hexes[0];
-      hexes.unshift({
-        'x': last.x+r,
-        'y': last.y
-      });
-    }
-    for (var i=0; i<=j; i++) { 
-      if (i == 2) break;
-      last = hexes[0];
-      hexes.unshift({
-        'x': last.x+r*c,
-        'y': last.y+r*s
-      });
-    }
-    if (j == 2) break;
-    for (var i=0; i<=j; i++) { 
-      last = hexes[0];
-      hexes.unshift({
-        'x': last.x-r*c,
-        'y': last.y+r*s
-      });
-    }
-    for (var i=0; i<=j; i++) { 
-      last = hexes[0];
-      hexes.unshift({
-        'x': last.x-r,
-        'y': last.y
-      });
-    }
+function loadBoard( gameid ) {
+  var game = {},
+    hexes = db.hmget(gameid+':hexes');
+  for (var dsrt=0, len=hexes.length, i=0; i<len; i++) {
+    game.board[i]['resource'] = hexes[i];
+    if (hexes[i] != 'desert') game.board[i]['chit'] = chits[i-dsrt];
+    else dsrt += 1;
+    if (chits[i-dsrt] == 6) game.board[i]['color'] = "maroon";
+    if (chits[i-dsrt] == 8) game.board[i]['color'] = "maroon";
   }
-  return hexes;
 }
 
-/*
- *  Fisher-Yates shuffle: http://bost.ocks.org/mike/shuffle/compare.html 
- */
-function shuffle( list ) { 
-  random.sequences({ 
-    "minimum": 0, 
-    "maximum": list.length-1 
-  }, function(error, data) { 
-    if (error) throw error; 
-    console.log(data); 
-  }); 
-  for (var temp=list.slice(0), i=list.length; i--; ) { 
-    list[data[i]] = temp[i]; 
+function createSequence( gameid, key, length ) {
+  if (length === undefined) {
+    console.log("cannot createSequence, length undefined");
   }
+  random.sequences({
+    "minimum": 0,
+    "maximum": length-1
+  }, function(err, data) {
+    if (err) throw err;
+    db.lpush(gameid+':'+key,data);
+  });
 }
